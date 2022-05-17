25d43b126d3423c248bca77baa09053b82f3707b
Arthur <arthur.holes@gmail.com>
2022-05-16 21:51:53 -0300

    first commit

---
+++ app/src/main/java/holes/arthur/simulator/MainActivity.kt
@@ -0,0 +1,11 @@
+package holes.arthur.simulator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
